256b82365371eaf813087f88b0dc4973cce9efb4
Jonathan Stockdill <jons@kenzanmedia.com>
2016-05-22 21:30:42 -0400

    Adding time out period

--- emoji.py
+++ emoji.py
@@ -1,6 +1,7 @@
 from picraft import World, Vector, Block, O, X, Y, Z, line, lines, filled
-from time import sleep
-from random import randint
+from time import sleep, time
+from random import randint,choice
+
 
 import ConfigParser
 
@@ -18,7 +19,7 @@ sleep(3)
 
 
 def eyes(player_pos):
-    height = player_pos + 15*Y
+    height = player_pos + 9*Y
     left_eye = height + -3*Z
     right_eye = height + 3*Z
 
@@ -31,7 +32,7 @@ def eyes(player_pos):
             eye + 1*Z + -1*Y]))
 
 def smile(player_pos):
-    height = player_pos + 9*Y
+    height = player_pos + 3*Y
 
     left_anchor = height - 3*Z
     right_anchor = height + 4*Z
@@ -41,6 +42,10 @@ def smile(player_pos):
     yield list(line(right_anchor, right_anchor + 2*Z + 2    *Y))
 
 
+def airfill(blocks, height):
+    for block in blocks:
+        airblock = block + height*Y
+        w.blocks[airblock] = Block('air')
 
 
 ## Render
@@ -59,12 +64,27 @@ def erase(blocks):
 
 
 #location to put genie
-pos = Vector(22,7,-3)
+pos = Vector(22,3,-3)
 print("pos: " + str(pos))
 
 ##define bounds
-low_corner = pos - 5*X - 5*Z
+low_corner =  pos - 5*X - 5*Z
 high_corner = pos + 5*X + 5*Z
+low_corner2 = pos + 5*X - 5*Z
+high_corner2 = pos - 5*X + 5*Z
+
+#fill surface with wool
+def prep_surface():
+    surface = []
+    for z in range(0, (high_corner.z - low_corner.z)):
+        surface.extend(list(line(low_corner + z*Z, low_corner2 + z*Z)))
+
+    w.blocks[surface] = Block('wool')
+
+    for y in range(1,20):
+        airfill(surface, y)
+
+prep_surface()
 
 ran_x = 0
 ran_z = 0
@@ -78,12 +98,12 @@ print("high_corner: " + str(high_corner))
 blocks = list(eyes(pos))
 blocks.extend(smile(pos))
 
-
+hide_time = 0
 while(True):
 
     ### Determine how to know if a host player exists
     player_pos = w.player.tile_pos
-    print(player_pos)
+
     if(low_corner.x < player_pos.x and
        low_corner.z < player_pos.z and
        high_corner.x > player_pos.x and
@@ -96,17 +116,34 @@ while(True):
 
            if(player_pos.x == ran_x and player_pos.z == ran_z):
                w.say("Congratulations!  Enjoy.  Come back soon!")
-               w.blocks[player_pos - Y] = Block('diamond_block')
+
+               gift = choice([
+                "diamond_block", "iron_block",
+                "emerald_block", "redstone_block", "lapis_block",
+                "gold_block", "coal_block"])
+
+               w.blocks[player_pos - Y] = Block(gift)
 
                erase(blocks)
-               state = "UNKNOWN"
-               sleep(120)
+               state = "ASK"
+               sleep(10)
+
+           elif(time() - hide_time > 15):
+               gift = choice(["melon_block","coal_block"])
+               w.blocks[player_pos - Y] = Block(gift)
+
+               w.say("Time\'s up.  Have a treat anyways")               
+
+               state = "ASK"
+               sleep(10)
 
        if(state == "ASK"):
            w.say("I've hidden a treat nearby.  Look for it.")
            ran_x = randint(low_corner.x+1,high_corner.x-1)
            ran_z = randint(low_corner.z+1,high_corner.z-1)
+           hide_time = time()
 
+           prep_surface()
            state = "HIDDEN"
 
        if(state == "UNKNOWN"):
